8c0dfcf51926bb7741053452e536a2cd840b02f6
viniciussilva512 <marvosvinicius128@gmail.com>
2023-11-27 13:37:10 -0300

    adicionei atividade22

---
+++ atv.py
@@ -0,0 +1,5 @@
+# Exercício Python 22: Faça um programa que mostre na tela os numeros pares entre 1 e 50 (dica estude range e seus paramentros)
+
+for lista in range(0,51,2):
+    print(lista)
+    
